82304acb888da0ea8530accc85f459d33d66bef8
Kunal Kumar <kunal2207kumar@gmail.com>
2025-12-30 17:19:44 +0530

    Update ChatsList.jsx

--- frontend/src/components/ChatsList.jsx
+++ frontend/src/components/ChatsList.jsx
@@ -17,7 +17,7 @@ function ChatsList() {
 
   return (
     <>
-      {chats.map((chat) => (
+      {Array.isArray(chats) && chats.map((chat) => (
         <div
           key={chat._id}
           className="bg-cyan-500/10 p-4 rounded-lg cursor-pointer hover:bg-cyan-500/20 transition-colors"
